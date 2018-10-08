8e68b5ada8a741182903ce48d3cba25bf551a144
Frank Sosa <frank.sosa@homepc.es>
2018-10-08 21:37:05 +0100

    arabic_to_roman() v1.1
    Segunda aproximación

--- 20180929/02_arabic_to_roman/arabic_to_roman.py
+++ 20180929/02_arabic_to_roman/arabic_to_roman.py
@@ -1,49 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding utf-8 -*-
 
+# arabic_to_roman v1.1
+# by Frank Sosa
+
 def arabic_to_roman(number_a):
   a_r = {'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1}
   ar_k = list(a_r.keys())
-  ar_v = list(a_r.values())
+
   number_r = ''
   
   miles = int(number_a/1000)
   centenas = int((number_a%1000)/100)
   decenas = int(((number_a%1000)%100)/10)
   unidades = int((((number_a%1000)%100)%10))
-  
-  # MILES
-  number_r = 'M'*miles
-  
-  #  CENTENAS
-  if centenas == 4:
-    number_r += 'CD'
-  elif centenas == 9:
-    number_r += 'CM'
-  elif centenas >= 5:
-    number_r += 'D'+ (centenas-5) * 'C'
-  else:
-    number_r += centenas * 'C'
-  
-  # DECENAS
-  if decenas == 4:
-    number_r = 'XL'
-  elif decenas == 9:
-    number_r += 'XC'
-  elif decenas >= 5:
-    number_r += 'L'+ (decenas-5) * 'X'
-  else:
-    number_r += decenas * 'X'
+  number = [miles,centenas,decenas,unidades]
 
-
-  # UNIDADES
-  if unidades == 4:
-    number_r += 'IV'
-  elif unidades == 9:
-    number_r += 'IX'
-  elif unidades >= 5:
-    number_r += 'V'+ (unidades-5) * 'I'
-  else:
-    number_r += unidades * 'I'
+  for index in range(len(number)):
+    idx = 2 * index
+    if number[index] == 4:
+      number_r += ar_k[idx] + ar_k[idx-1]
+    elif number[index] == 9:
+      number_r += ar_k[idx] + ar_k[idx-2]
+    elif number[index] >= 5:
+      number_r += ar_k[idx-1] + (number[index]-5) * ar_k[idx]
+    else:
+      number_r += number[index] * ar_k[idx]
 
   return number_r
