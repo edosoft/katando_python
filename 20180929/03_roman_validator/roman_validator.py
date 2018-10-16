4ba63c7cd890e9a047fed661c5d6702e294c1be2
Frank Sosa <frank.sosa@homepc.es>
2018-10-16 01:15:09 +0100

    roman_validator() v1.1 (comprehensions)
    Usamos el return con un 'comprehension'

--- 20180929/03_roman_validator/roman_validator.py
+++ 20180929/03_roman_validator/roman_validator.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# roman_validator() v1
+# roman_validator() v1.1
 # by Frank Sosa
 
 def roman_validator(number_r):
   import re
-  validator = False
+
   # Expresión regular de un número romano (v4.0)
   # M{0,3}(C(M|D)|DC{0,3}){0,1}(X(C|L)|LX{0,3}){0,1}(I(X|V)|VI{0,3}){0,1}
 
   roman_pattern = re.compile(r'\bM{0,3}(C(M|D)|DC{0,3})?(X(C|L)|LX{0,3})?(I(X|V)|VI{0,3})?\b',re.I)
   searching = roman_pattern.search(number_r)
 
-  if searching.group() != '':
-    validator = True
-  return validator
+  return True if searching.group() != '' else False 
